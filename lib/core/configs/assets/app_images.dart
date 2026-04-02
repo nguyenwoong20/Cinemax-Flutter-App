c661690c1eaffdc53e63ba73f3fa191b528c4765
Woong Ho Tuan Nguyen <nguyenwoong20@gmail.com>
2026-04-02 14:43:28 +0700

    feat: integrated Firebase Authentication with Google Sign-In

--- lib/core/configs/assets/app_images.dart
+++
@@ -1,4 +0,0 @@
-class AppImages {
-  static const basePath = 'assets/images/';
-  static const splashBackground = '${basePath}splash-bg.png';
-}
--- lib/core/usecase/usecase.dart
+++
@@ -1,4 +0,0 @@
-abstract class Usecase<type, Params> {
-  
-  Future<type> call({Params params});
-}
